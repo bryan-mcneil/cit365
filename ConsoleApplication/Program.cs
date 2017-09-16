74641d3b7fb3dad1d44de5ce215dcd827c65a7f2
Bryan McNeil <mcn09004@byui.edu>
2017-09-16 11:11:49 -0600

    Finished Console App Assignement.

--- ConsoleApplication/Program.cs
+++ ConsoleApplication/Program.cs
@@ -18,7 +18,23 @@ namespace ConsoleApplication
             Console.WriteLine(DateTime.Today);
             DateTime Christmas = new DateTime(2017, 12, 25);
             DateTime today = DateTime.Today;
-            Console.WriteLine("There is " + ((Christmas - today).Days - 1) + " days before Christmas");
+            Console.WriteLine("There is " + ((Christmas - today).Days) + " days before Christmas");
+            String mystring = String.Empty;
+
+            double width, height, woodLength, glassArea;
+            string widthString, heightString;
+            Console.WriteLine("Please enter a width: ");
+            widthString = Console.ReadLine();
+            width = double.Parse(widthString);
+            Console.WriteLine("Please enter a height: ");
+            heightString = Console.ReadLine();
+            height = double.Parse(heightString);
+            woodLength = 2 * (width + height) * 3.25;
+            glassArea = 2 * (width * height);
+            Console.WriteLine("The length of the wood is " +
+            woodLength + " feet");
+            Console.WriteLine("The area of the glass is " +
+            glassArea + " square metres");            Console.ReadLine();
         }
     }
 }
